b9669d794aacc406b3dadc9e579269b84f21e7dc
hariharan1009 <mdhari707@gmail.com>
2025-08-03 20:33:53 +0530

    03/08/25

---
+++ src/Helpful_Maths.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Helpful_Maths {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int[] a=new int[s
+
+    }
+}
