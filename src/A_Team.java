806ebf7f0bd3a61b5f709e07d9c7d4832d571ffe
hariharan1009 <mdhari707@gmail.com>
2025-08-26 22:36:02 +0530

    26/08/26

--- src/A_Team.java
+++ src/A_Team.java
@@ -4,10 +4,8 @@ public class A_Team {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-
         int co=0;
         while(n-->0){
-
             int a=sc.nextInt();
             int b=sc.nextInt();
             int c=sc.nextInt();
